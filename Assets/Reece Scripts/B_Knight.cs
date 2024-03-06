ec71da877c7212ef24eb2530ca94c088ba497895
Reece-E <144268748+Reece-E@users.noreply.github.com>
2024-03-06 12:01:48 -0800

    anim

--- Assets/Reece Scripts/B_Knight.cs
+++ Assets/Reece Scripts/B_Knight.cs
@@ -6,27 +6,28 @@ using UnityEngine;
 
 public class B_Knight : MonoBehaviour
 {
-    private Animator m_Animator;
+    Animator m_Animator;
     private float walkspeed;
     // Start is called before the first frame update
     void Start()
     {
         walkspeed = -0.30f;
-        GetComponent<Animator>();
+        m_Animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(walkspeed*Time.deltaTime, 0f, 0f, Space.Self);
+       
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        walkspeed = 0;
-        m_Animator.SetBool("interact", true);
-        //if()
-        //{
-        //}
+        if(other.CompareTag("test"))
+        {
+            walkspeed = 0;
+            m_Animator.SetBool("interact", true);
+        }
     }
 }
