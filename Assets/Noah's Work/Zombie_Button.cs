0bce50b55606c2c623d92c25bf5e47c98493ca45
Noah-Goodwin <144268611+Noah-Goodwin@users.noreply.github.com>
2024-03-14 11:40:36 -0700

    Lots of stuff Recee wasn't here

--- Assets/Noah's Work/Zombie_Button.cs
+++ Assets/Noah's Work/Zombie_Button.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class SelectButton : MonoBehaviour
+public class Zombie_Button : MonoBehaviour
 {
+    public GameObject prefab;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Instantiate(prefab, transform.position, quaternion.identity);
     }
+    
+    
+    
+    
 }
